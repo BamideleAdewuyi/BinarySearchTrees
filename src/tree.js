72bf5c2c1d05d14ede1428c47283f1649478ba9a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-24 20:24:06 +0100

    tree.js: Created root attribute and called buildTree in constructor

--- src/tree.js
+++ src/tree.js
@@ -3,10 +3,11 @@ import Node from "./Node.js";
 class Tree {
     constructor(array) {
         this.array = array;
+        this.root = this.buildTree(this.array);
     }
 
     buildTree(array) {
-        
+
     }
 }
 
