93c9a770ef4bcf25dd34fc627424ddd6d62c1529
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-02 15:39:21 +0100

    tree.js: Edited buildTree method so it takes start and end arguments

--- src/tree.js
+++ src/tree.js
@@ -3,16 +3,14 @@ import Node from "./Node.js";
 class Tree {
     constructor(array) {
         this.array = this.sortArr(this.removeDup(array));
-        this.root = this.buildTree(this.array);
+        this.root = this.buildTree(this.array, 0, this.array.length - 1);
     };
 
-    buildTree(array) {
-        let start = 0;
-        let end = array.length - 1;
+    buildTree(array, start, end) {
         if (start > end) return null;
         let mid = start + Math.floor((end - start) / 2);
-        let root = new Node(array[mid], this.buildTree(array.slice(start, mid)), this.buildTree(array.slice(mid+1, end+1)))
-        return root
+        let root = new Node(array[mid], this.buildTree(array, start, mid - 1), this.buildTree(array, mid + 1, end))
+        return root;
     };
 
     removeDup(array) {
