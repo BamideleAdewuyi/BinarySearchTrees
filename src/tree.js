02e4667cdb70793546a1880684a48a5da9bb10ae
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-08 00:52:46 +0100

    tree.js: Wrote prettyPrint method

--- src/tree.js
+++ src/tree.js
@@ -25,6 +25,19 @@ class Tree {
         })
     };
 
+    prettyPrint(node = this.root, prefix = '', isLeft = true) {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
+
     insertValue(root, value) {
         if (root === null) {
             return new Node(value, null, null)
